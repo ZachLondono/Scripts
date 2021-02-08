f411cdb6321c23ff20e3063209d3462bee6e1188
ZachLondono <zachary.londono5@gmail.com>
2021-02-08 04:06:56 -0500

    new switchAudio script written in python, shows device description instead of sink name

---
+++ switchAudio.py
@@ -0,0 +1,46 @@
+import os
+
+class Sink:
+	def __init__(self):
+		self.index = 0
+		self.name = ""
+		self.device = ""
+		self.isSelected = True
+
+
+out = os.popen('pacmd list-sinks').read()
+lines = out.split('\n')
+
+#sink_file = open("sinks")
+#lines = sink_file.readlines()
+
+sinks = []
+
+curr_sink = ""
+for line in lines:
+	if "index" in line :
+		curr_sink = Sink()
+		sinks.append(curr_sink)
+		index = line.split("index: ")[1]
+		curr_sink.index = int(index, base=10)
+
+	if "name: <" in line :
+		curr_sink.name = line.split("name: <")[1][0:-1]
+
+	if "device.description" in line :
+		curr_sink.device = line.split("device.description = ")[1]
+
+options = ""
+length = len(sinks)
+for i in range(length):
+	option = " {0} {1}".format(i, sinks[i].device)
+	options += option
+
+dialog = 'dialog --stdout --keep-tite --menu \"Choose Audio Sink\" 0 0 0'
+dialog += options
+
+out = os.popen(dialog).read()
+if out.isnumeric() :
+	index = int(out)
+	switch = "pacmd set-default-sink {0}".format(sinks[index].name)
+	os.system(switch)
